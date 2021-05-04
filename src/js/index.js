d62c966fd3e6118ec7c04a0702d5b5fe980a1e22
dbeccaria826 <beccdaniel@gmail.com>
2021-05-04 10:17:18 -0700

    drawAsteroids: added a draw functions for creating the asteroids, centered all elements on the page

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 import '../css/main.scss';
-import { drawShip } from './lib/drawing';
+import { drawShip, drawAsteroid } from './lib/drawing';
 import { drawGrid } from './lib/grid';
 
 const canvas = document.querySelector('#asteroids');
@@ -8,15 +8,15 @@ let radius = context.canvas.width * 0.1;
 
 
 drawGrid(canvas, context, '#000000', '#2C2C2C');
-let x, y, angle = 0;
-let w = context.canvas.width, h = context.canvas.height
-for(y = h/20; y < h; y += h/10) {
- for(x = w/20; x < w; x += w/10) {
-   context.save();
-   context.translate(x, y);
- context.rotate(angle);
- drawShip(context, w/30, {guide: true, lineWidth: 1});
- context.restore();
- angle = (angle + 0.0075 * Math.PI);
- }
-}
+// let x, y, angle = 0;
+// let w = context.canvas.width, h = context.canvas.height;
+// for(y = h/20; y < h; y += h/10) {
+//  for(x = w/20; x < w; x += w/10) {
+//    context.save();
+//    context.translate(x, y);
+//  context.rotate(angle);
+//  drawShip(context, w/30, {guide: true, lineWidth: 1});
+//  context.restore();
+//  angle = (angle + 0.0075 * Math.PI);
+//  }
+// }
--- src/js/lib/drawing.js
+++ src/js/lib/drawing.js
@@ -29,3 +29,25 @@ export const drawShip = (ctx, radius, options) => {
   ctx.stroke();
   ctx.restore();
 };
+
+export const drawAsteroid = (ctx, radius, segments, options) => {
+  options = options || {};
+  ctx.stokeStyle = options.stroke || 'white';
+  ctx.fillStyle = options.fill || 'black';
+  ctx.save();
+  ctx.beginPath();
+  for (let i = 0; i < segments; i++) {
+    ctx.rotate(2 * Math.PI / segments);
+    ctx.lineTo(radius, 0);
+  }
+  ctx.closePath();
+  ctx.fill();
+  ctx.stroke();
+  if (options.guide) {
+    ctx.lineWidth = 0.5;
+    ctx.beginPath();
+    ctx.arc(0, 0, radius, 0, 2 * Math.PI);
+    ctx.stroke();
+  }
+  ctx.restore();
+}
